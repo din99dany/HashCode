8d57fa46c7ed2626a6ac44f226cd9cdad3a50c52
Matei Daniel <din99dany@gmail.com>
2020-02-20 22:13:36 +0200

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -5,6 +5,11 @@
 #include <cmath>
 #include <vector>
 #include <utility> 
+#include <cstdio>
+#define NMAX 500001
+std::vector<bool> WasScaned( NMAX, false );
+int viz[NMAX];
+
 class Book 
 {
 public :
@@ -30,6 +35,13 @@ public :
 
 };
 
+bool CmpBooks( Book a, Book b )
+{
+    if ( a.GetScore() < b.GetScore() )
+        return true;
+    return false;
+}
+
 class Library
 {   
 public:
@@ -37,15 +49,17 @@ public:
     int Number_of_books;
     int Time_sign_up_process;
     int Books_shipped;
+    int id;
     std::vector<Book> Books;
 
     Library(){}
 
-    Library(int number_books, int time_sign_up, int number_books_ship)
+    Library(int mid, int number_books, int time_sign_up, int number_books_ship)
     {
         Number_of_books = number_books;
         Time_sign_up_process = time_sign_up;
         Books_shipped = number_books_ship;
+        id = mid;
     }
 
     void add_books(std::vector<Book> carti)
@@ -53,7 +67,34 @@ public:
         Books = carti;
     }
 
+    void SortBooks()
+    {
+        std::sort( Books.begin(), Books.end(), CmpBooks );
+    }
+
+    int get_score()
+    {
+        int Sum = 0;
+        int size = Books.size();
+        for (int i = 0; i < size; i++)
+        {
+            if (viz[Books[i].m_id] != 1)
+            {
+                Sum = Sum + Books[i].m_score;
+            }
+        }
+        return Sum;
+    }
+
 };
+
+bool cmpLibs( Library a, Library b )
+{
+
+    if ( a.get_score() > b.get_score() )
+        return true;
+    return false;
+}
 
 class Res
 {
@@ -70,21 +111,38 @@ public:
     }
 
     void add_book(int id)
-    {
+    {   
+        viz[ id ] = true;
         to_be_scanned.push_back(id);
+    }
+
+    void PrintRes()
+    {   
+        if ( to_be_scanned.empty() )
+            return;
+
+        std::cout << IDs << " " << to_be_scanned.size() << "\n";
+        for ( auto item : to_be_scanned )
+        {
+            std::cout<<item<<" ";
+        }
+        std::cout<<"\n";
     }
 };
 
 
 int main()
-{
+{   
 
+    freopen("out.txt","w",stdout);
     int noOfBooks, noOfLibraries, noOfDays;
+    int currentTime = 0;
+
     std::cin >> noOfBooks >> noOfLibraries >> noOfDays;
 
     std::vector<Book> books;
     std::vector<Library> libraries;
-
+  
     for ( int i = 0; i < noOfBooks; ++i )
     {
         int score;
@@ -96,7 +154,7 @@ int main()
     {
         int x, y, z;
         std :: cin >> x >> y >> z;
-        Library toAddLib( x, y, z );
+        Library toAddLib( i, x, y, z );
 
         std::vector< Book > toAddBooks;
         for ( int j = 0; j < x; ++j )
@@ -106,9 +164,53 @@ int main()
             toAddBooks.push_back( books[ idToAdd] );
         }
         toAddLib.add_books( toAddBooks );
+        toAddLib.SortBooks();
         libraries.push_back( toAddLib );
     }
 
+    std::sort( libraries.begin(), libraries.end(), cmpLibs );
+    std::vector< Res >  finalRes;
+    
+    while (  currentTime <  noOfDays && !libraries.empty() )
+    {   
+        auto selectedLib = libraries.front();
+        libraries.erase( libraries.begin() );
+
+        int scannedBooks = 0;
+        currentTime += selectedLib.Time_sign_up_process + 1;
+
+        Res result(selectedLib.id,0);
+
+        int copyTime = currentTime;
+        while ( selectedLib.Books.size() > 0 && copyTime <  noOfDays )
+        {   
+            for ( int time = 0; time < selectedLib.Books_shipped && !selectedLib.Books.empty(); ++time )
+            {   
+                auto toAddBook = selectedLib.Books.back();
+
+                selectedLib.Books.erase( selectedLib.Books.end() );
+                if ( viz[toAddBook.GetId()] )
+                {
+                    time--;
+                    continue;
+                }
+                result.add_book(toAddBook.GetId());
+            }  
+            copyTime++;
+        }
+        
+        if ( !result.to_be_scanned.empty() )
+            finalRes.push_back(result);
+
+        //result.PrintRes();
+        //std::sort( libraries.begin(), libraries.end(), cmpLibs );
+
+    }
+    std::cout << finalRes.size() << "\n";
+    for ( auto item : finalRes )
+    {
+        item.PrintRes();
+    }
 
 
     return 0;
