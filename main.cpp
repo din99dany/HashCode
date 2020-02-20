1c903a7b359b09454461653ec13a70f15ee91051
Codaru <codaruuu@gmail.com>
2020-02-20 20:03:18 +0200

    file

--- main.cpp
+++ main.cpp
@@ -1,10 +1,32 @@
 #include <iostream>
+class Book 
+{
+public :
+
+    int m_score;
+    int m_id;
+
+    Book(){
+    }
+
+    Book( int score, int id ) 
+    {
+        m_score = score;
+        m_id = id;
+    }
+
+
+    inline int GetScore() { return m_score; }
+    inline void SetScore( int newScore ) { m_score = newScore; }
+
+    inline int GetId() { return m_id; }
+    inline void SetId( int newId ) { m_id = newId; }
+
+};
+
+
 
 int main()
 {
-    int a, b;
-    std::cin >> a >> b;
-    std::cout << a + b << std::endl;
-    
     return 0;
 }
